82ce68de23c2e854bb54fc0bacf2c589a740957d
Beny Hdez <beny_hm@hotmail.com>
2026-04-07 14:14:21 -0700

    fix: all running

--- automation.js
+++ automation.js
@@ -2,167 +2,230 @@ const dotenv = require("dotenv");
 dotenv.config();
 var XLSX = require("xlsx");
 const puppeteer = require("puppeteer");
+const fs = require("fs");
 const workbook = XLSX.readFile(process.env.XLSX_PATH);
 const worksheet = workbook.Sheets[workbook.SheetNames[0]];
 const numerosGenerados = {};
 
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
 (async () => {
+  const browser = await puppeteer.launch({ headless: true }); //Change False to see the browser
+  var pages = await browser.pages();
+  const page = pages[pages.length - 1];
 
-    const browser = await puppeteer.launch({ headless: false });
-    var pages = await browser.pages();
-    const page = pages[pages.length - 1];
+  const username = process.env.USER_EMAIL;
+  const password = process.env.USER_PASSWORD;
 
-    const username = process.env.USER_EMAIL;
-    const password = process.env.USER_PASSWORD;
-    
+  // LOGIN
+  await page.goto(process.env.LOGIN_URL, {
+    waitUntil: "domcontentloaded",
+    timeout: 0,
+  });
+  await page.setViewport({ width: 1080, height: 1024 });
 
-    // LOGIN
-    await page.goto(process.env.LOGIN_URL, { waitUntil: 'load', timeout: 0 });
-    await page.setViewport({ width: 1080, height: 1024 });
+  // while (await page.$('#Username') === null) {
+  //     console.log('#Username Selector not found');
+  //     delaytime(1000);
+  // }
 
-    // while (await page.$('#Username') === null) {
-    //     console.log('#Username Selector not found');
-    //     delaytime(1000);
-    // }
+  if ((await page.$("#Username")) !== null) {
+    await page.waitForSelector("#Username");
+    await page.type("#Username", username);
+    await page.type("#txtPassword", password);
+    await page.click("#show_password");
+    await Promise.all([
+      page.waitForNavigation({ waitUntil: "domcontentloaded" }), // también puedes usar 'networkidle0' o 'domcontentloaded'
+      await page.click(".btn.btn-login"),
+    ]);
+  }
+  const prompt = require("prompt-sync")();
+  const vCode = prompt("Type Verification Code:");
+  console.log("Code: " + vCode);
 
-    if (await page.$('#Username') !== null) {
-        await page.waitForSelector('#Username');
-        await page.type('#Username', username);
-        await page.type('#txtPassword', password);
-        await page.click("#show_password");
-        await page.click(".btn.btn-login");
-    }
+  await page.waitForSelector("#verificationCode");
+  await page.type("#verificationCode", vCode);
+  await page.click("#verificationSubmit");
 
-    const menuSelector = `#tenant_apps [data-title="Voyager 7S"] a`;
+  const menuSelector = `#tenant_apps [data-title="Voyager 7S"] a`;
 
-    await page.waitForSelector(menuSelector);
-    if (await page.$(menuSelector) !== null) await page.click(menuSelector);
-    else console.log(menuSelector + ' not found');
+  await page.waitForSelector(menuSelector);
+  if ((await page.$(menuSelector)) !== null) await page.click(menuSelector);
+  else console.log(menuSelector + " not found");
 
-    await page.waitForNetworkIdle({ idleTime: 1000, timeout: 0 });
+  await page.waitForNetworkIdle({
+    waitUntil: "domcontentloaded",
+    idleTime: 1000,
+    timeout: 0,
+  });
 
-    // WEB PAGE 2
-    pages = await browser.pages();
-    const page2 = pages[pages.length - 1];
-    await page2.setViewport({ width: 1080, height: 1024 });
+  // WEB PAGE 2
+  pages = await browser.pages();
+  const page2 = pages[pages.length - 1];
+  await page2.setViewport({ width: 1080, height: 1024 });
 
-    await page2.waitForSelector('#cmdLogin');
-    // await page2.select('#loginAlias', 'Test'); // ENABLE FOR TESTING
-    if (await page2.$(`#cmdLogin`) !== null) await page2.click(`#cmdLogin`);
-    else console.log('cmdLogin not found');
+  await page2.waitForSelector("#cmdLogin");
+  // await page2.select('#loginAlias', 'Test'); // ENABLE FOR TESTING
+  if ((await page2.$(`#cmdLogin`)) !== null) await page2.click(`#cmdLogin`);
+  else console.log("cmdLogin not found");
 
-    page2.on('dialog', async dialog => {
-        await dialog.accept();
-    });
+  page2.on("dialog", async (dialog) => {
+    await dialog.accept();
+  });
 
-    await page2.waitForSelector('#top-menu-wrap');
-    while (await page2.$(`#top-menu-wrap`) === null) {
-        console.log('#top-menu-wrap not found');
-        timedelay(1000);
-    }
+  await page2.waitForSelector("#top-menu-wrap");
+  while ((await page2.$(`#top-menu-wrap`)) === null) {
+    console.log("#top-menu-wrap not found");
+    timedelay(1000);
+  }
 
-    for (let index = 2; index > 0; index++) {
-        try {
-            await page2.goto(process.env.ADD_CROS_ET, { waitUntil: 'load', timeout: 0 });
+  var arrayConsoleLog = [];
+  for (let index = 2; index > 0; index++) {
+    try {
+      await page2.goto(process.env.ADD_CROS_ET, {
+        waitUntil: "networkidle0",
+        timeout: 0,
+      });
 
-            if (await page2.$(`#BtnSave_Button`) !== null && await page2.$(`#BillingProperty_LookupCode`) !== null) {
-                const date = worksheet[`A${index}`].w;
-                const lookupCode = returnLookupCode(worksheet[`C${index}`].v);
-                const notes = worksheet[`D${index}`].v;
-                const chargeCode = worksheet[`E${index}`].v;
-                const amount = worksheet[`F${index}`].v;
-                const account = worksheet[`G${index}`].v;
-                const segment = worksheet[`H${index}`].v;
-                const payee = worksheet[`I${index}`].v;
-                const entity = 'cso';
-                const arrDate = date.split('/');
-                const numInvetario = generarNumeroInventario(lookupCode, date, arrDate, chargeCode);
-                // const numInvetario = `${lookupCode}${date.slice(-2)}M${arrDate[0]}${chargeCode.substring(0, 3).toUpperCase()}${randomNumber}`;
+      if (
+        (await page2.$(`#BtnSave_Button`)) !== null &&
+        (await page2.$(`#BillingProperty_LookupCode`)) !== null
+      ) {
+        const date = worksheet[`A${index}`].w;
+        const lookupCode = returnLookupCode(worksheet[`C${index}`].v);
+        const notes = worksheet[`D${index}`].v;
+        const chargeCode = worksheet[`E${index}`].v;
+        const amount = worksheet[`F${index}`].v;
+        const account = worksheet[`G${index}`].v;
+        const segment = worksheet[`H${index}`].v;
+        const payee = worksheet[`I${index}`].v;
+        const entity = "cso";
+        const arrDate = date.split("/");
+        const numInvetario = generarNumeroInventario(
+          lookupCode,
+          date,
+          arrDate,
+          chargeCode,
+        );
+        // const numInvetario = `${lookupCode}${date.slice(-2)}M${arrDate[0]}${chargeCode.substring(0, 3).toUpperCase()}${randomNumber}`;
 
-                console.log({
-                    Index: index,
-                    Date: date,
-                    LookupCode: lookupCode,
-                    Notes: notes,
-                    ChargeCode: chargeCode,
-                    Amount: amount,
-                    Account: account,
-                    Segment: segment,
-                    Payee: payee,
-                    Entity: entity,
-                    NumInventario: numInvetario
-                });
+        console.log({
+          Index: index,
+          Date: date,
+          LookupCode: lookupCode,
+          Notes: notes,
+          ChargeCode: chargeCode,
+          Amount: amount,
+          Account: account,
+          Segment: segment,
+          Payee: payee,
+          Entity: entity,
+          NumInventario: numInvetario,
+        });
 
-                // await page2.waitForNetworkIdle({ idleTime: 1000, timeout: 0 });
-                //Charge To
-                await page2.waitForNetworkIdle({ idleTime: 1000, timeout: 0 });
-                await page2.waitForSelector('#BillingProperty_LookupCode');
-                await page2.type('#BillingProperty_LookupCode', entity);
-                await page2.type('#BillingPerson_LookupCode', lookupCode.toLowerCase());
-                await page2.type('#ChargeCode_LookupCode', chargeCode);
-                await page2.click('#ChargeNotes_TextBox');
-                await page2.waitForSelector('#ChargeNotes_TextBox');
-                // await page2.waitForNetworkIdle({ idleTime: 1000, timeout: 0 });
-                await page2.type('#ChargeNotes_TextBox', notes);
+        //Charge To
+        // await page2.waitForNetworkIdle({ waitUntil: 'load', idleTime: 1000, timeout: 0 });
+        await page2.waitForSelector("#BillingProperty_LookupCode");
+        await page2.type("#BillingProperty_LookupCode", entity);
+        await page2.type("#BillingPerson_LookupCode", lookupCode.toLowerCase());
+        await page2.type("#ChargeCode_LookupCode", chargeCode);
+        await page2.click("#Body1");
+        // Aquí puede ocurrir un reload automático
+        await page2.waitForNavigation({
+          waitUntil: "networkidle0",
+          timeout: 0,
+        });
+        // Luego continúa
+        await page2.waitForSelector("#ChargeNotes_TextBox", { visible: true });
+        // await page2.click('#ChargeNotes_TextBox');
+        await page2.type("#ChargeNotes_TextBox", notes);
 
-                //Payable To
-                await page2.type('#BilledProperty_LookupCode', worksheet[`B${index}`].v.toLowerCase());
-                await page2.type('#BilledPerson_LookupCode', payee);
-                await page2.type('#PayableAccount_LookupCode', account);
-                await page2.type('#PayableInvNum_TextBox', numInvetario);
-                await page2.type('#PayableNotes_TextBox', notes);
+        //Payable To
+        await page2.type(
+          "#BilledProperty_LookupCode",
+          worksheet[`B${index}`].v.toLowerCase(),
+        );
+        await page2.type("#BilledPerson_LookupCode", payee);
+        await page2.type("#PayableAccount_LookupCode", account);
+        await page2.type("#PayableInvNum_TextBox", numInvetario);
+        await page2.type("#PayableNotes_TextBox", notes);
 
-                //Trans Batch Details
-                await page2.type('#TransDateOccrred_TextBox', date);
-                await page2.type('#TransPostDate_TextBox', `${arrDate[0]}/${arrDate[2]}`);
-                await page2.click('#Amount_TextBox', { clickCount: 3 });
-                await page2.waitForSelector('#Amount_TextBox');
-                await page2.type('#Amount_TextBox', amount.toString());
-                // await page2.waitForNetworkIdle({ idleTime: 1000, timeout: 0 });
-                //Payable Segment
-                await page2.type('#PayableSegment2_LookupCode', segment);
+        //Trans Batch Details
+        await page2.type("#TransDateOccrred_TextBox", date);
+        await page2.type(
+          "#TransPostDate_TextBox",
+          `${arrDate[0]}/${arrDate[2]}`,
+        );
+        await page2.click("#Amount_TextBox", { clickCount: 3 });
+        await page2.waitForSelector("#Amount_TextBox");
+        await page2.type("#Amount_TextBox", amount.toString());
+        // await page2.waitForNetworkIdle({ idleTime: 1000, timeout: 0 });
+        //Payable Segment
+        await page2.type("#PayableSegment2_LookupCode", segment);
 
-                //Charge Segment
-                await page2.type('#ChargeSegment1_LookupCode', lookupCode.toLowerCase());
+        //Charge Segment
+        await page2.type(
+          "#ChargeSegment1_LookupCode",
+          lookupCode.toLowerCase(),
+        );
 
-                // Finish - Save
-                await page2.click('#Body1');
-                await page2.click('#BtnSave_Button');
-                await page2.waitForNetworkIdle({ idleTime: 1000, timeout: 0 });
-            }
+        const messageContainer = await page2.$("#MessageTableContainer");
+        if (messageContainer !== null) {
+          const messageText = await page2.evaluate(() => {
+            const el = document.querySelector("#MessageTableContainer");
+            return el ? el.innerText.trim() : "";
+          });
 
-        } catch (e) {
-            console.log(e);
-            index = -1;
+          const logMessage = `⚠️ Index: ${index} - Posible error o advertencia.\n`;
+          fs.appendFileSync("console.log", logMessage);
+
+          arrayConsoleLog.push(logMessage.trim());
         }
+
+        // Finish - Save
+        // await page2.waitForNetworkIdle({ waitUntil: 'domcontentloaded', idleTime: 1000, timeout: 0 });
+        await page2.click("#Body1");
+        // Luego continúa
+        await page2.waitForSelector("#BtnSave_Button", { visible: true });
+        page2.click("#BtnSave_Button");
+        await page2.waitForNavigation({
+          waitUntil: "networkidle0",
+          timeout: 0,
+        });
+      }
+    } catch (e) {
+      console.log(e);
+      index = -1;
+      console.log(arrayConsoleLog);
     }
-    await browser.close();
+  }
+  await browser.close();
 })();
 
 function returnLookupCode(name) {
-    var arrayWords = name.toLowerCase().split(' ');
-    if (arrayWords[0] == "conrac") {
-        return arrayWords[arrayWords.length - 1];
-    } else {
-        return arrayWords[0].toLowerCase();
-    }
+  var arrayWords = name.toLowerCase().split(" ");
+  if (arrayWords[0] == "conrac") {
+    return arrayWords[arrayWords.length - 1];
+  } else {
+    return arrayWords[0].toLowerCase();
+  }
 }
 
 function generarNumeroInventario(lookupCode, date, arrDate, chargeCode) {
-    let randomNumber;
-    let numInventario;
+  let randomNumber;
+  let numInventario;
 
-    do {
-        // Generamos un número aleatorio (ajusta según sea necesario)
-        randomNumber = Math.floor(Math.random() * 100);
+  do {
+    // Generamos un número aleatorio (ajusta según sea necesario)
+    randomNumber = Math.floor(Math.random() * 100);
 
-        // Creamos el número de inventario
-        numInventario = `${lookupCode}${date.slice(-2)}M${arrDate[0]}${chargeCode.substring(0, 3).toUpperCase()}${randomNumber}`;
-    } while (numerosGenerados[numInventario]);
+    // Creamos el número de inventario
+    numInventario = `${lookupCode}${date.slice(-2)}M${arrDate[0]}${chargeCode.substring(0, 3).toUpperCase()}${randomNumber}`;
+  } while (numerosGenerados[numInventario]);
 
-    // Si el número es único, lo almacenamos en el diccionario
-    numerosGenerados[numInventario] = true;
+  // Si el número es único, lo almacenamos en el diccionario
+  numerosGenerados[numInventario] = true;
 
-    // Retornamos el número de inventario generado
-    return numInventario;
+  // Retornamos el número de inventario generado
+  return numInventario;
 }
